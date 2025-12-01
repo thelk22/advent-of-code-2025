44fbba1cb5c16cdd3817c7cdc61ae746086c8cde
Luke Kirwan <luke.kirwan@propelleraero.com.au>
2025-12-01 18:03:37 +1100

    add test ts-node file

---
+++ test.ts
@@ -0,0 +1,15 @@
+/*
+* A test file that can be run with ts-node for working out how things work in TypeScript.
+*/
+
+let res = 100 % 99;
+console.log(res);
+
+res = 99 % 99;
+console.log(res);
+
+res = 198 % 99;
+console.log(res);
+
+res = 3 % 99;
+console.log(res);
