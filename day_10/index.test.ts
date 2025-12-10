63b2a037bf49ce2735f8f6980a0da2bb20e19e26
Luke Kirwan <luke.kirwan@propelleraero.com.au>
2025-12-10 18:37:07 +1100

    djikstra for part b

--- day_10/index.test.ts
+++ day_10/index.test.ts
@@ -1,34 +1,33 @@
-import assert from "node:assert";
 import { describe, it } from "node:test";
-import { taskA } from ".";
+import { taskB } from ".";
 
 const inputFilePath = "day_10/input.txt";
 const testInputFilePath = "day_10/input.test.txt";
 
-describe("Run task A", () => {
-  it("with test input", () => {
-    const result = taskA(testInputFilePath);
-    console.log("Result: ", result);
-    assert.equal(result, 7);
-  });
-
-  it("with real input", () => {
-    const result = taskA(inputFilePath);
-    console.log("Result: ", result);
-    // We don't know the answer!
-  });
-});
-
-// describe("Run task B", () => {
+// describe("Run task A", () => {
 //   it("with test input", () => {
-//     const result = taskB(testInputFilePath);
+//     const result = taskA(testInputFilePath);
 //     console.log("Result: ", result);
-//     // assert.equal(result, 24);
+//     assert.equal(result, 7);
 //   });
 
 //   it("with real input", () => {
-//     const result = taskB(inputFilePath);
+//     const result = taskA(inputFilePath);
 //     console.log("Result: ", result);
 //     // We don't know the answer!
 //   });
 // });
+
+describe("Run task B", () => {
+  it("with test input", () => {
+    const result = taskB(testInputFilePath);
+    console.log("Result: ", result);
+    // assert.equal(result, 33);
+  });
+
+  // it("with real input", () => {
+  //   const result = taskB(inputFilePath);
+  //   console.log("Result: ", result);
+  //   // We don't know the answer!
+  // });
+});
--- day_10/index.ts
+++ day_10/index.ts
@@ -9,11 +9,13 @@ type Button = number[];
 type Instruction = {
   desiredLightConfig: LightConfig;
   buttons: Button[];
+  desiredJoltageConfig: number[];
 };
 
 type QueueNode = {
   lightConfig: LightConfig;
   nPresses: number;
+  joltageConfig: number[];
 };
 
 const pressButton = (currentLightConfig: number[], button: Button) => {
@@ -23,6 +25,13 @@ const pressButton = (currentLightConfig: number[], button: Button) => {
   return currentLightConfig.map(
     (lightOn, index) => (lightOn + button[index]) % 2
   );
+};
+
+const applyVoltage = (currentLightConfig: number[], button: Button) => {
+  if (currentLightConfig.length !== button.length) {
+    throw new Error("Button and light config length mismatch");
+  }
+  return currentLightConfig.map((lightOn, index) => lightOn + button[index]);
 };
 
 export function taskA(inputFilePath: string) {
@@ -77,7 +86,7 @@ export function taskA(inputFilePath: string) {
       // Use dictionary for fast lookups
       let visitedLightConfigs: { [id: string]: number } = {};
       let queue: QueueNode[] = [
-        { lightConfig: initialLightConfig, nPresses: 0 },
+        { lightConfig: initialLightConfig, nPresses: 0, joltageConfig: [] },
       ];
 
       while (queue.length > 0) {
@@ -120,6 +129,7 @@ export function taskA(inputFilePath: string) {
           .map((b) => ({
             lightConfig: pressButton(nodeToCheck.lightConfig, b),
             nPresses: nodeToCheck.nPresses + 1,
+            joltageConfig: [],
           }))
           .filter(({ lightConfig }) => {
             const lcId = lightConfig.join("");
@@ -138,4 +148,150 @@ export function taskA(inputFilePath: string) {
 
   return minPressesPerInstruction.reduce((a, b) => a + b, 0);
 }
-export function taskB(inputFilePath: string) {}
+export function taskB(inputFilePath: string) {
+  // PSEUDO CODE:
+
+  // BFS is now too slow for this problem because there are so many possible states
+  // (even if you limit the number of states to those between 0 and the desired voltage)
+
+  // So now we need to implement Dijkstra's algorithm
+
+  // Step 1: Parse the input into the light config and the buttons
+  // Step 2: Implement a function for applying a button press optimally
+  // Step 3: Initialise the light config to all off
+  // Step 4: Implement Dijkstra's algorithm to find the minimum button presses to reach the desired config
+
+  const input = readFileLines(inputFilePath).map((line) => {
+    const instructions = line.split(" ");
+
+    const desiredLightConfig = instructions[0]
+      .replace("[", "")
+      .replace("]", "")
+      .split("")
+      .map((c) => (c === "." ? 0 : 1));
+
+    const nLights = desiredLightConfig.length;
+
+    const buttons = instructions
+      .slice(1, -1)
+      .map((b) => b.replace("(", "").replace(")", "").split(",").map(Number))
+      .map((b) => {
+        let button = new Array(nLights).fill(0);
+        b.forEach((lightIndex) => {
+          button[lightIndex] = 1;
+        });
+        return button;
+      });
+
+    const desiredJoltageConfig = instructions[instructions.length - 1]
+      .replace("{", "")
+      .replace("}", "")
+      .split(",")
+      .map(Number);
+
+    return {
+      desiredLightConfig,
+      buttons,
+      desiredJoltageConfig,
+    } as Instruction;
+  });
+
+  // console.log("input", JSON.stringify(input));
+
+  const minPressesPerInstruction = input.map(
+    ({ buttons, desiredJoltageConfig }) => {
+      // console.log(
+      //   "Considering instruction with desired joltage config",
+      //   desiredJoltageConfig
+      // );
+      // console.log("desiredLightConfig", desiredLightConfig);
+      const desiredJoltageId = desiredJoltageConfig.join("");
+      const initialJoltageConfig = new Array(desiredJoltageConfig.length).fill(
+        0
+      );
+
+      let minPresses = Infinity;
+
+      // Use dictionary for fast lookups where ID is the joltage config as a string
+      // and value is the min presses (so far) to reach that config
+      let minJoltages: { [id: string]: number } = {};
+      let queue: QueueNode[] = [
+        { lightConfig: [], nPresses: 0, joltageConfig: initialJoltageConfig },
+      ];
+
+      while (queue.length > 0) {
+        // console.log("queue", queue);
+        // console.log("current queue length", queue.length);
+        // console.log("nodes visited so far", visitedLightConfigs);
+
+        // Pop next node from queue (the one with the lowest number of presses)
+        queue.sort((a, b) => a.nPresses - b.nPresses);
+        const nodeToCheck = queue[0];
+        queue = queue.slice(1);
+
+        console.log("Current queue", queue);
+        console.log(
+          "Number of nodes aware of cost",
+          Object.keys(minJoltages).length
+        );
+
+        // console.log("Checking node", nodeToCheck.lightConfig);
+        // console.log("Presses to reach node", nodeToCheck.nPresses);
+
+        if (!nodeToCheck) {
+          throw new Error("Queue empty unexpectedly");
+        }
+
+        if (nodeToCheck.joltageConfig.join("") === desiredJoltageId) {
+          minPresses = nodeToCheck.nPresses;
+          console.log(
+            "Desired joltage config reached with nPresses: ",
+            minPresses
+          );
+          break;
+        }
+
+        // Check if node has been visited
+        const nodeId = nodeToCheck.joltageConfig.join("");
+        if (nodeId in minJoltages) {
+          if (nodeToCheck.nPresses > minJoltages[nodeId]) {
+            // Already found a better or equal way to this node
+            console.log(
+              "Node already visited with a better or equal path",
+              nodeId
+            );
+            continue;
+          }
+        } else {
+          minJoltages[nodeId] = nodeToCheck.nPresses;
+        }
+
+        // Generate next nodes by pressing each button
+        // Only add them to the queue if they haven't been visited yet
+        // Only add them to the queue if the voltage is less than or equal to the desired voltage
+        const nextJoltageConfigs = buttons.map((b) => ({
+          lightConfig: [],
+          nPresses: nodeToCheck.nPresses + 1,
+          joltageConfig: applyVoltage(nodeToCheck.joltageConfig, b),
+        }));
+        // console.log("Generated next joltage configs:", nextJoltageConfigs);
+        nextJoltageConfigs.forEach(({ joltageConfig, nPresses }) => {
+          const jcId = joltageConfig.join("");
+          if (!minJoltages[jcId]) {
+            minJoltages[jcId] = nPresses;
+            queue.push({ lightConfig: [], nPresses, joltageConfig });
+            return;
+          }
+          if (nPresses < minJoltages[jcId]) {
+            minJoltages[jcId] = nPresses;
+            queue.push({ lightConfig: [], nPresses, joltageConfig });
+            return;
+          }
+        });
+      }
+
+      return minPresses;
+    }
+  );
+  return minPressesPerInstruction.reduce((a, b) => a + b, 0);
+}
